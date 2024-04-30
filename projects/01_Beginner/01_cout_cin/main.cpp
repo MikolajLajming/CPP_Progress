595d8c433e47ae64f2bf4488bc0993fc02236918
PAPA_SMERF <mikolaj.lajming@gmail.com>
2024-04-30 17:59:33 +0200

    correct variable initialization

--- projects/01_Beginner/01_cout_cin/main.cpp
+++ projects/01_Beginner/01_cout_cin/main.cpp
@@ -2,7 +2,7 @@
 
 using namespace std;
 int main() {
-    unsigned favourite_number;
+    unsigned favourite_number {0};
     cout << "Hello World! I'd like to know - what is your favourite number? (Please use positive integers only, I'm just a dumb program)" << endl;
     cin >> favourite_number;
     cout << "OMG! " << favourite_number << " is my favourite number too! Coincidence? I THINK NOT!" << endl;
