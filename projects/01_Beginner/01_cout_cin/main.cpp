c7d231e59702d0ce2c36cab5c0cf982d3b088072
PAPA_SMERF <mikolaj.lajming@gmail.com>
2024-04-30 15:34:41 +0200

    test

--- projects/01_Beginner/01_cout_cin/main.cpp
+++ projects/01_Beginner/01_cout_cin/main.cpp
@@ -1,10 +1,10 @@
 #include "main.h"
 
 using namespace std;
-int main() {
+void main() {
     unsigned favourite_number;
     cout << "Hello World! I'd like to know - what is your favourite number? (Please use positive integers only, I'm just a dumb program)" << endl;
     cin >> favourite_number;
     cout << "OMG! " << favourite_number << " is my favourite number too! Coincidence? I THINK NOT!" << endl;
-    return 0;
+
 }
