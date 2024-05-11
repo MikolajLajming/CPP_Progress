7f123253d6c80ba74157f141d1226017c44b19b7
PAPA_SMERF <mikolaj.lajming@gmail.com>
2024-05-12 00:47:52 +0200

    SUPER PROUD OF THIS ONE

--- projects/01_Beginner/06_build_a_tree_from_string/main.cpp
+++ projects/01_Beginner/06_build_a_tree_from_string/main.cpp
@@ -13,9 +13,9 @@ int main()
     for(size_t i{input.size()}; i > 0; --i){
         temp_string.clear();
         for(size_t j{1}; j < input.size(); ++j){
-            if(i <= j){
+            if(i <= j)
                 temp_string += input.at(j-i);
-            } else
+            else
                 temp_string += ' ';
         }
         for(size_t k {input.size()}; k > 0; --k){
