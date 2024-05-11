e7215534c06cdfb3600f00740cb1612eaa43cb8f
PAPA_SMERF <mikolaj.lajming@gmail.com>
2024-05-11 18:22:02 +0200

    just getting rid of std::

--- projects/01_Beginner/05_integer_list_exercise/main.cpp
+++ projects/01_Beginner/05_integer_list_exercise/main.cpp
@@ -1,29 +1,31 @@
 #include "main.h"
 
+using namespace std;
+
 int main() 
 { 
-    std::cout << "Hello, simple something here. We're gonna ask you to enter some integers and we'll put them into a list you can iteract with." << std::endl;
+    cout << "Hello, simple something here. We're gonna ask you to enter some integers and we'll put them into a list you can iteract with." << endl;
     int number_of_integers{0};
 
-    std::cout << "How many items in the list do you want to enter?: ";
-    std::cin >> number_of_integers;
+    cout << "How many items in the list do you want to enter?: ";
+    cin >> number_of_integers;
 
-    std::vector<int> items{};
+    vector<int> items{};
     int temp{0};
 
     if(number_of_integers > 0){
         for(int i{1}; i <= number_of_integers; ++i){
-            std::cout << "Please enter item no " << i << ": ";
-            std::cin >> temp;
+            cout << "Please enter item no " << i << ": ";
+            cin >> temp;
             items.push_back(temp);
         }
         temp = 0;
         number_of_integers = 0;
-        std::cout << "\nThe list looks like this now:\n" << std::endl
+        cout << "\nThe list looks like this now:\n" << endl
                     << "[ ";
         for(auto i: items)
-            std::cout << i << " ";
-        std::cout << "]" << std::endl;        
+            cout << i << " ";
+        cout << "]" << endl;        
     }
     
     char selection{0};
@@ -31,44 +33,44 @@ int main()
     bool done{false};
 
     do {
-        std::cout << "\nWhat do you want to do with the list?" << std::endl
-                    << "P - Print numbers" << std::endl
-                    << "A - Add a number" << std::endl
-                    << "M - Display mean of the numbers" << std::endl
-                    << "S - Display the smallest number" << std::endl
-                    << "L - Display the largest number" << std::endl
-                    << "\nQ - Quit" << std::endl;   
-        std::cin >> selection;
+        cout << "\nWhat do you want to do with the list?" << endl
+                    << "P - Print numbers" << endl
+                    << "A - Add a number" << endl
+                    << "M - Display mean of the numbers" << endl
+                    << "S - Display the smallest number" << endl
+                    << "L - Display the largest number" << endl
+                    << "\nQ - Quit" << endl;   
+        cin >> selection;
 
         switch(selection){
             case 'p':
             case 'P': 
-                std::cout << "\nPrinting numbers...\n" << std::endl
+                cout << "\nPrinting numbers...\n" << endl
                     << "[ ";
                 for(auto i: items)
-                    std::cout << i << " ";
-                std::cout << "]" << std::endl;
+                    cout << i << " ";
+                cout << "]" << endl;
                 break;
             case 'a':
             case 'A':
                 do{
-                    std::cout << "How many items in the list do you want to enter?: ";
-                    std::cin >> number_of_integers;
+                    cout << "How many items in the list do you want to enter?: ";
+                    cin >> number_of_integers;
                     if(number_of_integers <= 0)
-                        std::cout << "\nPlease enter positive integer!" << std::endl;
+                        cout << "\nPlease enter positive integer!" << endl;
                     else 
                         done = true;
                 } while(!done);
                 for(int i{1}; i <= number_of_integers; ++i){
-                    std::cout << "Please enter item no " << i << ": ";
-                    std::cin >> temp;
+                    cout << "Please enter item no " << i << ": ";
+                    cin >> temp;
                     items.push_back(temp);
                 }
-                std::cout << "\nThe list looks like this now:\n" << std::endl
+                cout << "\nThe list looks like this now:\n" << endl
                             << "[ ";
                 for(auto i: items)
-                    std::cout << i << " ";
-                std::cout << "]" << std::endl;                
+                    cout << i << " ";
+                cout << "]" << endl;                
                 number_of_integers = 0;
                 temp = 0;
                 done = false;
@@ -79,11 +81,11 @@ int main()
                     for(auto i: items)
                         mean += i;
                     mean /= items.size();
-                    std::cout << "The mean of all items in list is: "
-                                << mean << std::endl;
+                    cout << "\nThe mean of all items in list is: "
+                                << mean << endl;
                     mean = 0.0;                    
                 } else
-                    std::cout << "\nNo items in list!" << std::endl;
+                    cout << "\nNo items in list!" << endl;
                 break;
             case 's':
             case 'S':
@@ -92,12 +94,12 @@ int main()
                     for(auto i: items)
                         if(temp > i)
                             temp = i;
-                    std::cout << "\nThe smallest item in the list is: "
+                    cout << "\nThe smallest item in the list is: "
                                 // << "your PP xD"
-                                << temp << std::endl;
+                                << temp << endl;
                     temp = 0;
                 } else
-                    std::cout << "\nNo items in list!" << std::endl;                
+                    cout << "\nNo items in list!" << endl;                
                 break;
             case 'l':
             case 'L':
@@ -106,22 +108,22 @@ int main()
                     for(auto i: items)
                         if(temp < i)
                             temp = i;            
-                    std::cout << "\nThe largest item in the list is: "
+                    cout << "\nThe largest item in the list is: "
                                 // << "my PP (-_-)"
-                                << temp << std::endl;
+                                << temp << endl;
                     temp = 0;
                 } else
-                    std::cout << "\nNo items in list!" << std::endl;                
+                    cout << "\nNo items in list!" << endl;                
                 break;
             case 'q':
             case 'Q':
-                std::cout << "\nFINALLY! I'M FREE! BYE LADS!\n" << std::endl;
+                cout << "\nFINALLY! I'M FREE! BYE LADS!\n" << endl;
                 break;
-            default: std::cout << "\nUnknown selection, please try again\n" << std::endl;
+            default: cout << "\nUnknown selection, please try again\n" << endl;
         }
     } while (selection != 'q' && selection != 'Q');
 
-    std::cout << "\nBRAVO 6 going dark!\n" << std::endl;
+    cout << "\nBRAVO 6 going dark!\n" << endl;
     return 0;
 
 } 
