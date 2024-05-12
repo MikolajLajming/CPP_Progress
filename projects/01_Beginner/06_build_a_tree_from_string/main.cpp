f7e8238e862e6d885029a0d9e50770274274b15b
PAPA_SMERF <mikolaj.lajming@gmail.com>
2024-05-12 13:29:25 +0200

    SUPER PROUD OF THIS ONE

--- projects/01_Beginner/06_build_a_tree_from_string/main.cpp
+++ projects/01_Beginner/06_build_a_tree_from_string/main.cpp
@@ -4,36 +4,22 @@ using namespace std;
 
 int main() 
 {
-
     string input{};
     getline(cin, input);
-
     string temp_string{};
-
     for(size_t i{input.size()}; i > 0; --i){
-
-        temp_string.clear();
-
+        temp_string.clear();        
         for(size_t j{1}; j < input.size(); ++j){
-
             if(j < i)
                 temp_string += ' ';
             else
                 temp_string += input.at(j-i);
-
         }
-
         for(size_t k {input.size()}; k > 0; --k){
-
             if(k >= i)
                 temp_string += input.at(k-i);
-
         }
-
-        cout << temp_string << endl;
-        
+        cout << temp_string << endl;       
     }
-
     return 0;
-
 } 
